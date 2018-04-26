54431c10901ea0c027dd5ecc06920fc324f7d1dd
Anwesh43 <anweshthecool0@gmail.com>
2018-04-26 07:44:55 +0530

    adding LineToArrowView in Activity

--- app/src/main/java/ui/anwesome/com/kotlinlinetoarrowview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlinetoarrowview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinlinetoarrowview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.linetoarrowview.LineToArrowView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LineToArrowView.create(this)
     }
 }
